ddc5ad69fec1c79d7355d76b631b9f096154fcd6
hammadtariq838 <24100267@lums.edu.pk>
2024-03-15 15:27:26 +0500

    fix: production api base url

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const BASE_URL =
-    import.meta.env.MODE === 'development' ? 'http://localhost:5000' : 'https://smart-meet.findyourtutor.co.uk'
+    import.meta.env.MODE === 'development' ? 'http://localhost:5000' : 'https://bidai-api.hammad-tariq.me'
 
 export const USERS_URL = `${BASE_URL}/api/users`
