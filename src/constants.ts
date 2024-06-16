31a78b6df7a171f315664b05534d41924ace43f8
hammadtariq838 <24100267@lums.edu.pk>
2024-06-16 14:37:43 +0500

    fix: backend api url

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,6 @@
 export const BASE_URL =
-    import.meta.env.MODE === 'development' ? 'http://localhost:5000' : 'https://bidai-api.hammad-tariq.me'
+  import.meta.env.MODE === 'development'
+    ? 'http://localhost:5000'
+    : 'https://api.bidai.hammadtariq.me';
 
-export const USERS_URL = `${BASE_URL}/api/users`
+export const USERS_URL = `${BASE_URL}/api/users`;
